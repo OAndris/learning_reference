aef4e0de8449625350b27671a0124024c33eb620
Andras Orban <orbanandris@gmail.com>
2020-09-11 18:43:32 +0200

    End-to-End test example

--- testing_and_tdd/testing_javascript_academind/util.test.js
+++ testing_and_tdd/testing_javascript_academind/util.test.js
@@ -1,3 +1,4 @@
+const puppeteer = require("puppeteer");
 const { generateText, checkAndGenerate } = require("./util");
 
 // Unit test example:
@@ -16,3 +17,32 @@ test("should output data-less text", () => {
 test("should generate a valid text output", () => {
   expect(checkAndGenerate("Max", 29)).toBe("Max (29 years old)");
 });
+
+// End-to-End test (user interface test) example:
+test("should create an element with text and correct class", async () => {
+  const headless = false; // NOTE: choose between normal and headless running
+
+  const browser = await puppeteer.launch(
+    headless
+      ? { headless: true }
+      : {
+          headless: false,
+          slowMo: 80,
+          args: ["--start-maximized"],
+          // args: ["--window-size=1920,1080"],
+        }
+  );
+  const page = await browser.newPage();
+  await page.goto(
+    "http://127.0.0.1:5500/testing_and_tdd/testing_javascript_academind/index.html"
+  );
+
+  await page.click("input#name");
+  await page.type("input#name", "Anna");
+  await page.click("input#age");
+  await page.type("input#age", "28");
+  await page.click("#btnAddUser");
+
+  const finalText = await page.$eval(".user-item", (el) => el.textContent);
+  expect(finalText).toBe("Anna (28 years old)");
+}, 10000); // NOTE: increase timeout milliseconds if needed
