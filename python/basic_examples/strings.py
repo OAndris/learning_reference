25f2698cccebd1ef40b48d5887b984037534edb3
Andras Orban <orbanandris@gmail.com>
2020-09-23 05:38:23 +0200

    Add string example

---
+++ python/basic_examples/strings.py
@@ -0,0 +1,35 @@
+
+x = 'Hello, reißverschluss!'
+print(x.upper())
+print(x.lower())
+print(x.casefold())
+print(x.capitalize())
+print(x.title())
+print(x.swapcase())
+print()
+
+phrase = 'Make it work, make it right, make it fast.'
+print(phrase)
+print(' - '.join(phrase.split(', ')))
+print(phrase.replace(', ', ' - '))
+print('. '.join([substring.capitalize() for substring in phrase.split(', ')]))
+print(f'Number of "make" in the phrase: {phrase.count("make")}')
+print()
+
+x = 5
+y = 17
+print('{} {}'.format(x, y))
+print('{0} {1}'.format(x, y))
+print('{1} {0}'.format(x, y))
+print('{1} {0} {1}'.format(x, y))
+print('{a} {b}'.format(a=x, b=y))
+print('{a:*<5} {b:+>5}'.format(a=x, b=y))  # left-adjust and right-adjust
+print()
+print(f'{x} {y}')  # f-strings (mode modern string formatting since Python v3.6, it is built on the format method and is simply a shortcut for it)
+print(f'{x:*<5} {y:.>10}')  # left-adjust and right-adjust
+print(f'{123456789:,}')  # thousands limiter
+print(f'{123456789:,}'.replace(',', '.'))
+print(f'{123456789/1e6:.4f}')  # precision
+print(f'{123456789:x}')  # hexadecimal
+print(f'{123456789:o}')  # octal
+print(f'{123456789:b}')  # binary
