a0bfe227fedc5f3e5c4e1af381cac76c1bacb880
Andras Orban <orbanandris@gmail.com>
2020-10-09 06:34:56 +0200

    Event handling (using the v-on directive, or its shortand, such as just @click='method')

--- vue/main.js
+++ vue/main.js
@@ -6,9 +6,26 @@ const app = Vue.createApp({
             inventory: 0,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                { id: 2234, color: 'green' },
-                { id: 2235, color: 'blue' },
+                {
+                    id: 2234,
+                    color: 'green',
+                    image: './assets/images/socks_green.jpg',
+                },
+                {
+                    id: 2235,
+                    color: 'blue',
+                    image: './assets/images/socks_blue.jpg',
+                },
             ],
+            cart: 0,
         };
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1;
+        },
+        updateImage(variantImage) {
+            this.image = variantImage;
+        },
     },
 });
