92061a8a35c00fd8859c74e09ab76bc7e6b94410
Andras Orban <orbanandris@gmail.com>
2020-10-09 06:24:22 +0200

    List rendering

--- vue/main.js
+++ vue/main.js
@@ -4,7 +4,11 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_green.jpg',
             inventory: 0,
-            onSale: true,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, color: 'green' },
+                { id: 2235, color: 'blue' },
+            ],
         };
     },
 });
