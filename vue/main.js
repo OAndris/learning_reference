60cac556c851e915408d9dd2d570af2e299d6f60
Andras Orban <orbanandris@gmail.com>
2020-10-09 06:17:35 +0200

    Conditional rendering of elements with the v-if, v-else-if, v-else directives

--- vue/main.js
+++ vue/main.js
@@ -3,6 +3,8 @@ const app = Vue.createApp({
         return {
             product: 'Socks',
             image: './assets/images/socks_green.jpg',
+            inventory: 0,
+            onSale: true,
         };
     },
 });
