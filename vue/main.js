e7a103943c1f1e9cf9a0bc3ab07aed61f0b82d13
Andras Orban <orbanandris@gmail.com>
2020-10-09 06:08:12 +0200

    Attribute binding (e.g. v-bind:src, or just :src)

--- vue/main.js
+++ vue/main.js
@@ -2,7 +2,7 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            description: 'Awesome socks',
+            image: './assets/images/socks_green.jpg',
         };
     },
 });
