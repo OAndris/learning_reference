3efdc75265c15fe7e80482cbaba0f27c82aadaee
Andras Orban <orbanandris@gmail.com>
2020-10-10 08:42:36 +0200

    Computed properties

--- vue/main.js
+++ vue/main.js
@@ -1,31 +1,51 @@
-const app = Vue.createApp({
+const vueApp = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            image: './assets/images/socks_green.jpg',
-            inventory: 0,
+            brand: 'Vue Mastery',
+            selectedVariant: 0,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
                 {
                     id: 2234,
                     color: 'green',
                     image: './assets/images/socks_green.jpg',
+                    quantity: 50,
+                    onSale: true,
                 },
                 {
                     id: 2235,
                     color: 'blue',
                     image: './assets/images/socks_blue.jpg',
+                    quantity: 0,
+                    onSale: false,
                 },
             ],
             cart: 0,
         };
     },
+    computed: {
+        title() {
+            return `${this.brand} ${this.product}`;
+        },
+        image() {
+            return this.variants[this.selectedVariant].image;
+        },
+        quantity() {
+            return this.variants[this.selectedVariant].quantity;
+        },
+        onSale() {
+            return this.variants[this.selectedVariant].onSale
+                ? `${this.title} is on sale!`
+                : '';
+        },
+    },
     methods: {
         addToCart() {
             this.cart += 1;
         },
-        updateImage(variantImage) {
-            this.image = variantImage;
+        updateVariant(index) {
+            this.selectedVariant = index;
         },
     },
 });
