26c8edd78975e30f677fb3ef0a8ebc4e505f7cce
Andras Orban <orbanandris@gmail.com>
2020-10-11 08:45:49 +0200

    Create AddTodo component and possibility to add todos

--- vue/vue_crash_course_by_traversy/src/App.vue
+++ vue/vue_crash_course_by_traversy/src/App.vue
@@ -1,6 +1,7 @@
 <template>
     <div id="app">
         <Header />
+        <AddTodo v-on:add-todo="addTodo" />
         <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
     </div>
 </template>
@@ -8,11 +9,13 @@
 <script>
 import Header from './components/layout/Header';
 import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
 
 export default {
     name: 'App',
     components: {
         Header,
+        AddTodo,
         Todos
     },
     data() {
@@ -37,6 +40,9 @@ export default {
         };
     },
     methods: {
+        addTodo(newTodo) {
+            this.todos = [...this.todos, newTodo];
+        },
         deleteTodo(id) {
             this.todos = this.todos.filter(todo => todo.id !== id);
         }
@@ -53,5 +59,16 @@ export default {
 body {
     font-family: Arial, Helvetica, sans-serif;
     line-height: 1.4;
+}
+.btn {
+    display: inline-block;
+    border: none;
+    background: #555;
+    color: #fff;
+    padding: 7px 20px;
+    cursor: pointer;
+}
+.btn:hover {
+    background: #666;
 }
 </style>
