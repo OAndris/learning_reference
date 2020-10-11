4c7457efb9f2be74e0d22251484ea0dbc9cdbb0d
Andras Orban <orbanandris@gmail.com>
2020-10-11 09:38:07 +0200

    Replace hard coded data with calls to API (jsonplaceholder)

--- vue/vue_crash_course_by_traversy/src/App.vue
+++ vue/vue_crash_course_by_traversy/src/App.vue
@@ -10,6 +10,7 @@
 import Header from './components/layout/Header';
 import Todos from './components/Todos';
 import AddTodo from './components/AddTodo';
+import axios from 'axios';
 
 export default {
     name: 'App',
@@ -20,32 +21,32 @@ export default {
     },
     data() {
         return {
-            todos: [
-                {
-                    id: 1,
-                    title: 'Todo One',
-                    completed: false
-                },
-                {
-                    id: 2,
-                    title: 'Todo Two',
-                    completed: true
-                },
-                {
-                    id: 3,
-                    title: 'Todo Three',
-                    completed: false
-                }
-            ]
+            todos: []
         };
     },
     methods: {
         addTodo(newTodo) {
-            this.todos = [...this.todos, newTodo];
+            const { title, completed } = newTodo;
+            axios
+                .post('https://jsonplaceholder.typicode.com/todos', {
+                    title,
+                    completed
+                })
+                .then(res => (this.todos = [...this.todos, res.data]))
+                .catch(err => console.log(err));
         },
         deleteTodo(id) {
-            this.todos = this.todos.filter(todo => todo.id !== id);
+            axios
+                .delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+                .then(() => (this.todos = this.todos.filter(todo => todo.id !== id)))
+                .catch(err => console.log(err));
         }
+    },
+    created() {
+        axios
+            .get('https://jsonplaceholder.typicode.com/todos?_limit=5')
+            .then(res => (this.todos = res.data))
+            .catch(err => console.log(err));
     }
 };
 </script>
--- vue/vue_crash_course_by_traversy/src/components/AddTodo.vue
+++ vue/vue_crash_course_by_traversy/src/components/AddTodo.vue
@@ -8,8 +8,6 @@
 </template>
 
 <script>
-import { v4 as uuidv4 } from 'uuid';
-
 export default {
     name: 'AddTodo',
     data() {
@@ -21,7 +19,6 @@ export default {
         addTodo(e) {
             e.preventDefault();
             const newTodo = {
-                id: uuidv4(),
                 title: this.title,
                 completed: false
             };
