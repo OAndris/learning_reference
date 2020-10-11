ec262c2c403cc5ee006443261fbfe5f552a66031
Andras Orban <orbanandris@gmail.com>
2020-10-11 08:23:58 +0200

    Add header component

--- vue/vue_crash_course_by_traversy/src/App.vue
+++ vue/vue_crash_course_by_traversy/src/App.vue
@@ -1,15 +1,18 @@
 <template>
     <div id="app">
+        <Header />
         <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
     </div>
 </template>
 
 <script>
+import Header from './components/layout/Header';
 import Todos from './components/Todos';
 
 export default {
     name: 'App',
     components: {
+        Header,
         Todos
     },
     data() {
--- vue/vue_crash_course_by_traversy/src/components/Todos.vue
+++ vue/vue_crash_course_by_traversy/src/components/Todos.vue
@@ -1,6 +1,5 @@
 <template>
     <div>
-        <h1>Todos</h1>
         <div v-for="todo in todos" v-bind:key="todo.id">
             <TodoItem v-bind:todo="todo" v-on:del-todo="$emit('del-todo', todo.id)" />
         </div>
