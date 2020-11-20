fda4bfef19f452385c49bb033646e64441fda833
Andras Orban <orbanandris@gmail.com>
2020-11-20 18:47:24 +0100

    Create the first endpoint, used by the server for sending messages and by the frontend to displaying those

--- node/server/server.js
+++ node/server/server.js
@@ -4,8 +4,15 @@ const app = express(); // instantiate an Express server
 
 app.use(express.static(__dirname)); // serve a static file
 
+const messages = [
+    { name: 'Tim', message: 'Hi' },
+    { name: 'Jane', message: 'Hello' },
+];
+
+app.get('/messages', (req, res) => {
+    res.send(messages);
+}); // handle GET requests sent to the specified route (with a callback that gives us access to both the request and the response)
+
 const server = app.listen(3000, () => {
     console.log(`Server is listening on port ${server.address().port}`);
 }); // start the Express server, listening for requests on a specified port
-
-// insert data into HTML using jQery (and bind to certain events)
