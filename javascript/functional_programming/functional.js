30f37c0da8ea6e44af4f37bf96b3f873c2af9432
Andras Orban <orbanandris@gmail.com>
2020-12-07 20:22:14 +0100

    Add the individual functions for functional programming example

--- javascript/functional_programming/functional.js
+++ javascript/functional_programming/functional.js
@@ -20,6 +20,62 @@ Benefits:
 Source of example: https://www.udemy.com/course/advanced-javascript-concepts/
 */
 
+//=====================================================
+// Data
+//=====================================================
+const user = {
+    name: 'Kim',
+    active: true,
+    cart: [],
+    purchases: [],
+};
+
+const item = {
+    name: 'laptop',
+    price: 200,
+};
+
+const log = [];
+
+//=====================================================
+// Individual functions
+//=====================================================
+function addItemToCart(user, item) {
+    log.push(user);
+    log.push('Add item to cart');
+    const updatedCart = user.cart.concat(item);
+    return Object.assign({}, user, { cart: updatedCart });
+}
+
+function applyTaxToItems(user) {
+    log.push(user);
+    log.push('Apply tax to items');
+    const { cart } = user;
+    const taxRate = 1.3;
+    const updatedCart = cart.map((item) => {
+        return {
+            name: item.name,
+            price: item.price * taxRate,
+        };
+    });
+    return Object.assign({}, user, { cart: updatedCart });
+}
+
+function buyItem(user) {
+    log.push(user);
+    log.push('Buy item');
+    return Object.assign({}, user, { purchases: user.cart });
+}
+
+function emptyCart(user) {
+    log.push(user);
+    log.push('Empty the cart');
+    return Object.assign({}, user, { cart: [] });
+}
+
+//=====================================================
+// Combined functions
+//=====================================================
 function combineFunctions(...fns) {
     const pipe = (f, g) => (...args) => g(f(...args));
     // const compose = (f, g) => (...args) => f(g(...args));
