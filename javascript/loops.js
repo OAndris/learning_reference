ae24068b56ff3877a0516ddec9f1ec5f74b16f86
Andras Orban <orbanandris@gmail.com>
2020-12-22 20:54:03 +0100

    Create JavaScript loops examples

--- javascript/loops.js
+++ javascript/loops.js
@@ -1,24 +1,99 @@
 /*
 Example functions demonstrating loops in JavaScript:
-- while
 - for
-
-And keywords:
-- continue
-- break
-- return
-
-TODO:
 - for in
 - for of
-- forEach
+- while
 - do while
+
+And some related Array methods:
+- Array.forEach
+- Array.map
+
+And keywords that can be used in loops:
+- continue (continue to the next iteration, skipping the rest of the current iteration)
+- break (break out of the current loop)
+- return (return from the current function)
 
 Sources:
 - https://www.linkedin.com/learning/javascript-essential-training-3/break-and-continue-loops
 - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Loops_and_iteration
 - https://www.w3schools.com/js/js_loop_for.asp
 */
+
+function forExample() {
+    // Repeat executing a statement for as long as the specified condition evaluates to true.
+    console.log('FOR LOOP');
+    for (let step = 0; step < 5; step++) {
+        console.log(step);
+    }
+}
+
+function forInExample() {
+    // Iterate over the properties of an object (shouldn't be used with arrays - it would normally return its numeric indexes, but also any user-defined properties, if the array was modified in such a way)
+    console.log('FOR IN LOOP');
+    const obj = {
+        A: 1,
+        B: 2,
+    };
+    for (let property in obj) {
+        console.log(property);
+    }
+}
+
+function forOfExample() {
+    // Iterate over the iterable objects (including Array, Map, Set, arguments)
+    console.log('FOR OF LOOP');
+    const arr = [3, 5, 7];
+    for (let element of arr) {
+        console.log(element);
+    }
+}
+
+function whileExample() {
+    // Repeat executing a statement for as long as the specified condition evaluates to true.
+    console.log('WHILE LOOP');
+    let i = 0;
+    while (i < 5) {
+        i += 1;
+        console.log(i);
+    }
+}
+
+function doWhileExample() {
+    // Execute a statement and then repeat it for as long as the specified condition evaluates to true.
+    console.log('DO WHILE LOOP');
+    let i = 0;
+    do {
+        i += 1;
+        console.log(i);
+    } while (i < 5);
+}
+
+function arrayForEachExample() {
+    // Array method for executing a provided function once for each array element.
+    console.log('ARRAY.FOREACH EXAMPLE');
+    const arr = [3, 5, 7];
+    arr.forEach((element) => console.log(element));
+}
+
+function arrayMapExample() {
+    // Array method for creating a new array populated with the results of a callback function called on every element in the calling array.
+    console.log('ARRAY.MAP EXAMPLE');
+    const arr = [3, 5, 7];
+    const newArr = arr.map((element) => 2 * element);
+    console.log(newArr);
+}
+
+forExample();
+forInExample();
+forOfExample();
+whileExample();
+doWhileExample();
+arrayForEachExample();
+arrayMapExample();
+
+//=================================
 
 function chance() {
     const MIN = 0;
@@ -58,5 +133,5 @@ function prime() {
     }
 }
 
-chance();
-prime();
+// chance();
+// prime();
