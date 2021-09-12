b98dcb75bd2dc503faf9dd210e68384042d3ddf8
Andras Orban <orbanandris@gmail.com>
2021-09-12 19:41:42 +0200

    Update comments about currying

--- javascript/currying.js
+++ javascript/currying.js
@@ -3,6 +3,7 @@ Currying is the technique of translating the evaluation of a function that takes
 into evaluating a sequence of functions, each with a single argument.
 
 Currying is useful for creating multiple, reusable utility functions that remember the parameter with which they were created.
+It's also highly useful for creating pipelines (with "compose" or "pipe"), which need a chain of functions, each having a single input (and a single output, that becomes the input of the next function in the chain)
 
 Source of example code: https://www.udemy.com/course/complete-react-developer-zero-to-mastery/learn/lecture/15176902#overview
 */
