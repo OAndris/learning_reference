f41bc016925a7c3e3ad3044be22d06e1674f63f3
Andras Orban <orbanandris@gmail.com>
2022-08-08 18:39:20 +0200

    small improvements

--- general_concepts/design_patterns/facade_pattern/facade_pattern.js
+++ general_concepts/design_patterns/facade_pattern/facade_pattern.js
@@ -2,25 +2,43 @@
  * ----------------------
  * "Facade" pattern:
  * ----------------------
- * The idea is to create a "facade" between your complex code (e.g. an API) and your actual business logic code, making it easier to use the complex code without having to worry about its implementation details.
+ * The idea is to create a "facade" between your complex code that includes implementation details (e.g. the use of a specific API) and your actual business logic code.
  *
  * Benefits:
- * - it leads to a much cleaner codebase (by providing elegant, clean, simple interfaces even if the underlying logic is potentially complex and maybe even ugly)
- * - it allows us to use complex code easily (hides away the complexities that are unrelated to the actual business logic)
- * - and most importantly, it also makes it a lot easier to change the implementation, i.e. replace any part of that complex code (because the actual implementation detail is in just a single location, not all over the codebase). For example, if "fetch" is never used directly in our code, only a facade that implements "fetch", then it becomes a lot easier to switch to using "axios" inside that single part of the codebase, where it is implemented.
+ * - use complex code easily, by hiding away the complexities and implementation details that are unrelated to the actual business logic
+ * - much cleaner codebase, by providing elegant, clean, simple interfaces even if the underlying logic is potentially complex and maybe even ugly
+ * - and most importantly, it becomes significantly easier to change the implementation (e.g. switch from fetch API to axios), because the actual implementation detail is in just a single location, not all over the codebase
+ *
+ * Example:
+ * If a facade is created that implements "fetch" and is used throughout the codebase, then it becomes a lot easier to switch to using "axios" inside that single part of the codebase, where it is implemented.
  *
  * Based on: "Facade Pattern - Design Patterns" by Web Dev Simplified (https://www.youtube.com/watch?v=fHPa5xzbpaA&list=PLZlA0Gpn_vH_CthENcPCM0Dww6a5XYC7f&index=5)
  */
 
+// ====================================================================================================================
+// High-level code, containing business logic. Easy to use, all the complexities and implementation details are hidden:
+// ====================================================================================================================
 function getUsers() {
-    return getAxios('https://jsonplaceholder.typicode.com/users'); // also works with getAxios
+    return getWithAxios('https://jsonplaceholder.typicode.com/users'); // also works with getWithFetch
 }
 
 function getUserPosts(userId) {
-    return getFetch('https://jsonplaceholder.typicode.com/posts', { userId }); // also works with getAxios
+    return getWithFetch('https://jsonplaceholder.typicode.com/posts', { userId }); // also works with getWithAxios
 }
 
-function getFetch(url, params = {}) {
+getUsers().then((users) => {
+    users.forEach((user) => {
+        getUserPosts(user.id).then((posts) => {
+            console.log(user.name);
+            console.log(posts.length);
+        });
+    });
+});
+
+// ==============================================================================================================
+// Low-level code, "facade" containing implementation details that aren't directly related to the business logic:
+// ==============================================================================================================
+function getWithFetch(url, params = {}) {
     // This function is the "facade", that provides an implementation of the fetch function, and hides away any unnecessary/unrelated complexities from the rest of our codebase.
     // It makes an otherwise potentially complex logic easy to use, helps to avoid repetitions, and allows us to easily change the implementation (e.g. replace a used library) in just a single location.
     const queryString = Object.entries(params)
@@ -34,9 +52,9 @@ function getFetch(url, params = {}) {
     }).then((res) => res.json());
 }
 
-function getAxios(url, params = {}) {
+function getWithAxios(url, params = {}) {
     // This function demonstrates how easy it is, thanks to the Facade pattern, to refactor the implementation, e.g. switch from fetch API to axios.
-    // Notice how the interface of the original function doesn't change so the usage isn't affected and the rest of the codebase require no change.
+    // Notice how the interface of the original function (getWithFetch) doesn't change, so the usage isn't affected and the rest of the codebase requires no change.
     const queryString = Object.entries(params)
         .map(([paramKey, paramValue]) => `${paramKey}=${paramValue}`)
         .join('&');
@@ -44,12 +62,3 @@ function getAxios(url, params = {}) {
     console.log(finalUrl);
     return axios({ url, method: 'GET', params }).then((res) => res.data);
 }
-
-getUsers().then((users) => {
-    users.forEach((user) => {
-        getUserPosts(user.id).then((posts) => {
-            console.log(user.name);
-            console.log(posts.length);
-        });
-    });
-});
--- general_concepts/design_patterns/single_responsibility_principle/single_responsibility_principle.js
+++ general_concepts/design_patterns/single_responsibility_principle/single_responsibility_principle.js
@@ -1,17 +1,19 @@
 /**
  * ----------------------
- * "Single Responsibility Principle" design pattern:
+ * "Single Responsibility Principle" design pattern (1st part of SOLID):
  * ----------------------
- * All classes, functions, modules, etc. should have just a single responsibility, i.e. it should only ever have one single reason to change.
- * In other words, all of these code parts should do just a single thing, but do that well.
- * It makes it a lot easier to find, read and understand the code that needs to be changed for a specific task.
+ * Software entities (classes, functions, modules, etc.) should have just a single reason to change.
+ * That is, an entity should implement logic for a single task only.
+ * If it needs to perform more than one task, then the logic corresponding to those other tasks should be outsourced into a new entity that handles just that logic (and can be called/used wherever it's needed).
+ * Following this principle significantly improves maintainability and readability, makes tests more straightforward, and even make it easier to review (in source control) what part of the codebase has changed.
  *
  * Based on: "Single Responsibility Principle Explained - SOLID Design Principles" by Web Dev Simplified (https://www.youtube.com/watch?v=UQqY3_6Epbg&list=PLZlA0Gpn_vH9kocFX7R7BAe_CvvOCO_p9)
  */
 
+// Logging-related logic is outsourced to a separate module. If the logger needs to be changed, only the code of that module changes, the CalorieTracker class remains the same.
 const logMessage = require('./logger.js');
 
-// This class is only responsible for tracking changes to calories. Logging related to logic is outsourced to a separate module.
+// This class is only responsible for tracking changes to calories. And the only reason to change its code is if the tracking-related logic itself changes.
 class CalorieTracker {
     constructor(maxCalories) {
         this.maxCalories = maxCalories;
@@ -19,7 +21,7 @@ class CalorieTracker {
     }
     trackCalories(calorieCount) {
         this.currentCalories += calorieCount;
-        logMessage(`Recorded ${calorieCount} calories.`);
+        logMessage(`Recorded ${calorieCount} calories.`); // the logger is called here but implemented in its own module
         if (this.currentCalories > this.maxCalories) {
             logMessage('Max calories exceeded!');
         }
